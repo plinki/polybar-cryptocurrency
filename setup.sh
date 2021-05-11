0e7905d1f3a32048a21d05c7e9ce621b874051f8
plinki <geolazaria@gmail.com>
2021-05-11 23:37:08 +0300

    paths wrong

--- setup.sh
+++ setup.sh
@@ -5,19 +5,20 @@ DISTRO=$(lsb_release -i | cut -f 2-)
 
 case $DISTRO in
     'Arch')
-        dir_fonts=~/.local/share/fonts
+        dir_fonts=/home/$USER/.local/share/fonts/
         ;;
     'Artix')
-        dir_fonts=~./local/share/fonts
+        dir_fonts=/home/$USER/.local/share/fonts/
         ;;
     *)
-        dir_fonts=~/.fonts
+        dir_fonts=/home/$USER/.fonts/
         ;;
 esac
 
-dir_polybar=~/.config/polybar/
+        mkdir $dir_fonts
 
-mkdir -p $dir_fonts
+dir_polybar=/home/$USER/.config/polybar/
+
 
 cp coins.otf $dir_fonts
 cp -t $dir_polybar pcrypto.py coins.svg
